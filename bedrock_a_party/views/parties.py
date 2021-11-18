ae9ccf6889a2876262422dad57ebbcafae7c5de5
@rudysemola <r.semola@studenti.unipi.it>
2021-11-18 18:13:49 +0100

    code for all parties, pass test1_party

--- bedrock_a_party/views/parties.py
+++ bedrock_a_party/views/parties.py
@@ -1,3 +1,5 @@
+import random
+
 from flakon import JsonBlueprint
 from flask import abort, jsonify, request
 
@@ -6,31 +8,45 @@ from bedrock_a_party.classes.party import CannotPartyAloneError, Party
 parties = JsonBlueprint('parties', __name__)
 
 _LOADED_PARTIES = {}  # dict of available parties
+_LOADED_PARTIES["loaded_parties"] = []
 _PARTY_NUMBER = 0  # index of the last created party
 
 
 # TODO: complete the decoration
-@parties.route("/parties")
+@parties.route("/parties", methods=["POST", "GET"])
 def all_parties():
     result = None
+    global _PARTY_NUMBER
+    global _LOADED_PARTIES
     if request.method == 'POST':
         try:
             # TODO: create a party
+            req_data = request.get_json()
+            # Create the id
+            if len(_LOADED_PARTIES["loaded_parties"]) != 0:
+                _PARTY_NUMBER += 1
+            new_party = Party(_PARTY_NUMBER, req_data["guests"])
+            # track the new party
+            _LOADED_PARTIES["loaded_parties"].append(new_party.serialize())
+            result = jsonify({"party_number": _PARTY_NUMBER})
         except CannotPartyAloneError:
             # TODO: return 400
+            result = "Record not found", 400
 
     elif request.method == 'GET':
         # TODO: get all the parties
+        result = _LOADED_PARTIES
 
     return result
 
 
 # TODO: complete the decoration
-@parties.route("/parties/loaded")
+@parties.route("/parties/loaded", methods=['GET'])
 def loaded_parties():
     # TODO: returns the number of parties currently loaded in the system
+    return jsonify({'loaded_parties': len(_LOADED_PARTIES["loaded_parties"])})
 
-
+"""
 # TODO: complete the decoration
 @parties.route("/party/<id>")
 def single_party(id):
@@ -116,3 +132,4 @@ def exists_party(_id):
         abort(404)  # error 404: Not Found, i.e. wrong URL, resource does not exist
     elif not(_id in _LOADED_PARTIES):
         abort(410)  # error 410: Gone, i.e. it existed but it's not there anymore
+"""
